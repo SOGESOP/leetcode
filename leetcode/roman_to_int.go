5d16921ab127966e6461f693f2286ac03f69fe62
Theodore Ogonah <theoogonah@gmail.com>
2023-09-16 22:49:11 +0100

    working on romans to int

--- leetcode/roman_to_int.go
+++ leetcode/roman_to_int.go
@@ -1,22 +1,19 @@
 package main
 
-import(
-	"fmt"
-
-)
 // a map is the result of applying a function to an array?
 // a map can aslo be described as taking several things and associating them with another thing
 
 func romanToInt(s string) int {
-	type roman_char struct {
-		value int
-	}
-	var I string = roman_char{value: 1}
-	var V string = roman_char{value: 5}
-	var X string = roman_char{value: 10}
-	var L string = roman_char{value: 50}
-	roman_char_array := []{I, V, X, L}
+	roman_char := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000}
+	for i := 0; i < len(s); i++ {
 
-	var roman_chars = []rune{'I', 'V', 'X', 'L', 'C', 'D', 'M'}
+	}
 
 }
