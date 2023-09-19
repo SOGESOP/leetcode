81f7ac0f807101acad0fb74f78946a56083428bc
SOGESOP <theoogonah@gmail.com>
2023-09-19 19:10:25 +0100

    finished prefix, very slow

--- leetcode/long_prefix.go
+++ leetcode/long_prefix.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func longestCommonPrefix(strs []string) string {
-	// maks the current string
+
+	// dont work on leetcode
+	// if slices.Contains(strs, "") {
+	// 	return ""
+	// }
+
+	// checks for empty string
+	for _, s := range strs {
+		if s == "" {
+			return ""
+		}
+	}
 
 	// finds the longest word in the list, and then uses that as the reference point
 	var inital_prefix string
@@ -18,20 +33,20 @@ func longestCommonPrefix(strs []string) string {
 		var longest_prefix string
 		for idx, x := range word {
 			if x != rune(inital_prefix[idx]) {
-				return longest_prefix
+				break
 			} else {
 				longest_prefix += string(x)
 			}
-			prefix_list = append(prefix_list, string(x))
 		}
+		prefix_list = append(prefix_list, longest_prefix)
 	}
-	inital_prefix = ""
+	fmt.Printf("%v", prefix_list)
+	// starts from longest possible prefix and keeps going until the shortest one is reached, and finds the shortest one
 	for _, x := range prefix_list {
-		if len(x) > len(inital_prefix) {
+		if len(x) < len(inital_prefix) {
 			inital_prefix = x
 		}
 	}
 
 	return inital_prefix
-
 }
