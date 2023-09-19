98e1777f88e5132f17c5d41d92dec0340d0cc488
SOGESOP <theoogonah@gmail.com>
2023-09-19 18:34:00 +0100

    contiue longest prefix go

--- leetcode/long_prefix.go
+++ leetcode/long_prefix.go
@@ -1,27 +1,37 @@
 package main
 
-import(
-	"sort"
-)
-
 func longestCommonPrefix(strs []string) string {
-	var ascending_list=sort.Strings()
 	// maks the current string
-	var current_prefix string = ascending_list[len(ascending_list)-1]
-	for _, word:=range sts{
 
-		current_prefix=
-		for idx, x:= range word{
-			if x
-
-			}
-
-
+	// finds the longest word in the list, and then uses that as the reference point
+	var inital_prefix string
+	for _, x := range strs {
+		if len(x) > len(inital_prefix) {
+			inital_prefix = x
 		}
 	}
 
+	// this will store all the prefixes that are created
+	prefix_list := []string{}
+	// checks each word in the list against the longest
+	for _, word := range strs {
+		var longest_prefix string
+		for idx, x := range word {
+			if x != rune(inital_prefix[idx]) {
+				return longest_prefix
+			} else {
+				longest_prefix += string(x)
+			}
+			prefix_list = append(prefix_list, string(x))
+		}
+	}
+	inital_prefix = ""
+	for _, x := range prefix_list {
+		if len(x) > len(inital_prefix) {
+			inital_prefix = x
+		}
+	}
 
+	return inital_prefix
 
-	var return_thing string
-	return return_thing
 }
