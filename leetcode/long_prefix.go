f49bc4e395856bd20b2450419dde77b920894eb7
SOGESOP <theoogonah@gmail.com>
2023-09-19 17:01:16 +0100

    start common prefix

--- leetcode/long_prefix.go
+++ leetcode/long_prefix.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import(
+	"sort"
+)
 
+func longestCommonPrefix(strs []string) string {
+	var ascending_list=sort.Strings()
+	// maks the current string
+	var current_prefix string = ascending_list[len(ascending_list)-1]
+	for _, word:=range sts{
+
+		current_prefix=
+		for idx, x:= range word{
+			if x
+
+			}
+
+
+		}
+	}
+
+
+
+	var return_thing string
+	return return_thing
 }
